553e41ee57b1987d0d93fbf21f47e8c384ccac33
Dmytro Kryvonos <kdv.dimak@gmail.com>
2026-08-02 14:03:22 +0300

    Homework-9.

--- HM 11. Operators/js/login.js
+++
@@ -1,9 +0,0 @@
-let email = "user@gmail.com";
-let password = "123321123";
-let isEmailVerified = true;
-
-  let  canLogin = email !== "" && password !== "" && isEmailVerified;
-
-    if (canLogin === true){
-        console.log("Логін успішний");
-    } else console.log("Перевірте дані");
